6ad4d92c69e87443d4f4519e466e8d5e73079e98
Nicolas Perez <52212129+pznicolasm@users.noreply.github.com>
2019-12-17 00:35:55 -0300

    Update busqueda-binaria.py

--- busqueda-binaria.py
+++ busqueda-binaria.py
@@ -1,18 +1,18 @@
-def busquedaBinaria (lista, item):
+def busquedaBinaria (lista, valor):
     if len(lista) == 0:
         return False
     else:
         medio = len(lista) // 2
-        if lista[medio] == item:
+        if lista[medio] == valor:
             return True
         else:
-            if item < lista[medio]:
-                return busquedaBinaria(lista[:medio], item)
+            if valor < lista[medio]:
+                return busquedaBinaria(lista[:medio], valor)
             else:
-                return  busquedaBinaria(lista[medio:], item)
+                return  busquedaBinaria(lista[medio:], valor)
 
 listaprueba = [1,2,3,6,8,9,7,15,22,63,77,89,99]
 
-item = 15
+valor = 15
 
 print(busquedaBinaria(listaprueba, 99))
